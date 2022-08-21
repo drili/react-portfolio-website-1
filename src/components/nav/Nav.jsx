f53168b5144290463c5130f034e037cd7253b190
drili <DriliB@hotmail.com>
2022-08-21 20:46:33 +0200

    :zap: Finished Fixed Navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./nav.css"
+import {AiOutlineHome, AiOutlineUser} from "react-icons/ai"
+import {BiBook, BiMessageSquareAdd} from "react-icons/bi"
+import {RiServiceLine} from "react-icons/ri"
+import { useState } from 'react'
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState("#")
+
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiOutlineHome></AiOutlineHome></a>
+            <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><AiOutlineUser></AiOutlineUser></a>
+            <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBook></BiBook></a>
+            <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><RiServiceLine></RiServiceLine></a>
+            <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageSquareAdd></BiMessageSquareAdd></a>
+        </nav>
     )
 }
 
